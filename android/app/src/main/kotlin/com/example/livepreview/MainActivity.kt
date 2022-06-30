35c743e3cf1c3deac329107220065d24bf502dc8
GauravAnand563 <gauravanand563@gmail.com>
2022-07-01 00:14:31 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/livepreview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.livepreview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final kh1 = GoogleFonts.roboto(
+    color: Colors.white,
+    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w500));
+final kh2 = GoogleFonts.poppins(
+    color: Colors.white,
+    textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold));
+final kh3 = GoogleFonts.sourceSansPro(
+    color: Colors.white,
+    textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w300));
+final kh4 = GoogleFonts.notoSans(
+    color: Colors.white,
+    textStyle: TextStyle(fontSize: 17, fontWeight: FontWeight.w400));
